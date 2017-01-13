46fcd1677610c8fdd2d2b9fda444362e4d8b191e
haicheng0516 <haicheng0516@163.com>
2017-01-13 14:24:17 +0800

    指纹解锁/指纹验证

--- FingerprintDemo/ViewController.m
+++ FingerprintDemo/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+- (IBAction)yanzhengBtn:(UIButton *)sender;
 
 @end
 
@@ -19,11 +21,103 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)yanzhengBtn:(UIButton *)sender {
+    
+    LAContext *context = [[LAContext alloc]init];
+    
+    NSError *error = nil;
+    NSString *result = @"需要您的支付密码进行支付";
+    context.localizedFallbackTitle = @"快捷支付";
+    NSLog(@"data before authentication == %@",[context evaluatedPolicyDomainState]);
+    
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:result reply:^(BOOL success, NSError * _Nullable error) {
+           
+            if (success) {
+                
+                NSLog(@"验证成功");
+                NSLog(@"data after authentication = %@",[context evaluatedPolicyDomainState]);
+            }else{
+            
+                NSLog(@"%@",error.localizedDescription);
+                switch (error.code) {
+                    case LAErrorSystemCancel:
+                        NSLog(@"Authentication was cancelled by the system");
+                         //切换到其他APP，系统取消验证Touch ID
+                        break;
+                    case LAErrorUserCancel:
+                    {
+                        NSLog(@"Authentication was cancelled by the user");
+                        //用户取消验证Touch ID
+                        break;
+                    }
+                    case LAErrorUserFallback:
+                    {
+                        NSLog(@"User selected to enter custom password");
+                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                            //用户选择输入密码，切换主线程处理
+                        }];
+                        break;
+                    }
+                    case LAErrorAuthenticationFailed:
+                    {
+                        NSLog(@"Authentication Failed");
+                        break;
+                    }
+                    case LAErrorTouchIDLockout:
+                    {
+                        NSLog(@"TOUCH ID is locked out");
+                        break;
+                    }
+                    case LAErrorAppCancel:
+                    {
+                        NSLog(@"app cancle the authentication");
+                        break;
+                    }
+                    case LAErrorInvalidContext:
+                    {
+                        NSLog(@"context is invalidated");
+                        break;
+                    }
+                    default:
+                    {
+                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                            //其他情况，切换主线程处理
+                        }];
+                        break;
+                    }
+                }
+            }
+        }];
+    }else{
+    
+        NSLog(@"%@",error.localizedDescription);
+        //不支持指纹识别，LOG出错误详情
+        switch (error.code) {
+            case LAErrorTouchIDNotEnrolled:
+            {
+                NSLog(@"TouchID is not enrolled");
+                break;
+            }
+            case LAErrorPasscodeNotSet:
+            {
+                NSLog(@"A passcode has not been set");
+                break;
+            }
+            default:
+            {
+                NSLog(@"TouchID not available");
+                break;
+            }
+        }
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 
 @end
